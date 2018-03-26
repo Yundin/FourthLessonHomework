413b2fb589472a40cd9b2db9d2901fa6ded4bf4e
Yundin Vladislav <yundin@styleru.net>
2018-03-26 03:10:14 +0300

    Initial commit

---
+++ app/src/main/java/com/yundin/recyclerexample/MainActivity.java
@@ -0,0 +1,25 @@
+package com.yundin.recyclerexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler);
+
+    }
+}
